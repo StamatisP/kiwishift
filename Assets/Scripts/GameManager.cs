bdeadd53f73817a8c9a8b71976d82facb9f6adc9
Stamos Prekas <prekasstamos@gmail.com>
2018-11-22 17:46:44 -0500

    Basic phase shifting implementation
    Holy shit this was hard
    
    I hate life

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,10 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Tilemaps;
 
 public class GameManager : MonoBehaviour
 {
-
+	#region singleton stuff
 	private static GameManager _instance;
 
 	public static GameManager Instance
@@ -20,8 +24,6 @@ public class GameManager : MonoBehaviour
 		}
 	}
 
-	public bool isShifted { get; set; }
-
 	// Use this for initialization
 	void Awake ()
 	{
@@ -32,6 +34,14 @@ public class GameManager : MonoBehaviour
 		}
 		else Destroy (this);
 	}
+	#endregion 
+	public bool isShifted { get; set; }
+	public List<GameObject> normalWorldObjects;
+	public List<GameObject> otherWorldObjects;
+	public List<Tilemap> normalWorldRenderers;
+	public List<Tilemap> otherWorldRenderers;
+	public List<TilemapCollider2D> normalWorldColliders;
+	public List<TilemapCollider2D> otherWorldColliders;
 
 	// Update is called once per frame
 	void Start ()
@@ -40,9 +50,95 @@ public class GameManager : MonoBehaviour
 		//DontDestroyOnLoad();
 	}
 
+	//[MenuItem ("Testing/Call Level Load")]
+	public void LevelLoad ()
+	{
+		StartCoroutine ("LevelLoadCoroutine");
+	}
+
+	IEnumerator LevelLoadCoroutine ()
+	{
+		print ("loading level...");
+		SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex + 1);
+		yield return new WaitForFixedUpdate ();
+
+		// levelload should get called when you hit the exit portal, and when you click "start game" on main menu
+		normalWorldObjects.Clear ();
+		otherWorldObjects.Clear ();
+		normalWorldRenderers.Clear ();
+		otherWorldRenderers.Clear ();
+
+		List<GameObject> normalWorldObjectsList = GameObject.FindGameObjectsWithTag ("NormalWorld").ToList ();
+		foreach (GameObject _go in normalWorldObjectsList)
+		{
+			Debug.Log ("loop work");
+			print (_go.name);
+			normalWorldObjects.Add (_go);
+			normalWorldRenderers.Add (_go.GetComponent<Tilemap> ());
+			normalWorldColliders.Add (_go.GetComponent<TilemapCollider2D> ());
+		}
+		// this is so spaghetti
+
+		List<GameObject> otherWorldObjectsList = GameObject.FindGameObjectsWithTag ("OtherWorld").ToList ();
+		foreach (GameObject _go in otherWorldObjectsList)
+		{
+			Debug.Log ("loop work");
+			print (_go.name);
+			otherWorldObjects.Add (_go);
+			otherWorldRenderers.Add (_go.GetComponent<Tilemap> ());
+			otherWorldColliders.Add (_go.GetComponent<TilemapCollider2D> ());
+		}
+
+		yield return null;
+	}
+
+	//[MenuItem ("Testing/Call Phase Shift")]
 	public void PhaseShift ()
 	{
-		isShifted = !isShifted;
+		isShifted = !isShifted; // if false, then in normal world. if true, then other world
 		print ("shifting phase " + isShifted);
+
+		// the sprite renderer should be cached...
+		// did it, cached above
+
+		// this won't work really good, i need to somehow reset the colliders and color
+		if (isShifted == false)
+		{
+			foreach (Tilemap _rend in otherWorldRenderers)
+			{
+				_rend.color = new Color (1f, 1f, 1f, 0.5f);
+				// wait shit its not gonna reset oh
+			}
+			foreach (TilemapCollider2D _coll in otherWorldColliders)
+			{
+				_coll.enabled = false;
+			}
+		}
+		else if (isShifted == true)
+		{
+			foreach (Tilemap _rend in normalWorldRenderers)
+			{
+				_rend.color = new Color (1f, 1f, 1f, 0.5f);
+			}
+			foreach (TilemapCollider2D _coll in normalWorldColliders)
+			{
+				_coll.enabled = false;
+			}
+		}
+
+		/* if (isShifted == false)
+		{
+			foreach (var obj in otherWorldObjects)
+			{
+				obj.GetComponent<SpriteRenderer> ().color = new Color (1f, 1f, 1f, 0.5f);
+			}
+		}
+		else if (isShifted == true)
+		{
+			foreach (var obj in normalWorldObjects)
+			{
+				obj.GetComponent<SpriteRenderer> ().color = new Color (1f, 1f, 1f, 0.5f);
+			}
+		}*/
 	}
 }
