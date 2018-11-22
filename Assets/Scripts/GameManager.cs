e83b3b8ad4693dbf24815a6bf018554eebf385f2
Stamos Prekas <prekasstamos@gmail.com>
2018-11-22 14:10:44 -0500

    Basic implementation of phase shift
    New gamemanager simpleton, that shifts phases. The player shouldn't dictate how the phase shifts.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+	private static GameManager _instance;
+
+	public static GameManager Instance
+	{
+		get
+		{
+			if (_instance == null)
+			{
+				GameObject go = new GameObject ("GameManager");
+				go.AddComponent<GameManager> ();
+			}
+			return _instance;
+		}
+	}
+
+	public bool isShifted { get; set; }
+
+	// Use this for initialization
+	void Awake ()
+	{
+		if (_instance == null)
+		{
+			_instance = this;
+			DontDestroyOnLoad (gameObject);
+		}
+		else Destroy (this);
+	}
+
+	// Update is called once per frame
+	void Start ()
+	{
+		isShifted = false;
+		//DontDestroyOnLoad();
+	}
+
+	public void PhaseShift ()
+	{
+		isShifted = !isShifted;
+		print ("shifting phase " + isShifted);
+	}
+}
