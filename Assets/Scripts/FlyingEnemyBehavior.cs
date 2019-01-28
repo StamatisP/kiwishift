58dc65092ac1622040ef390c7522fe2ef86c65d7
Kevin43 <Kevin43@users.noreply.github.com>
2019-01-28 10:36:11 -0500

    Holy fuck so much

--- Assets/Scripts/FlyingEnemyBehavior.cs
+++ Assets/Scripts/FlyingEnemyBehavior.cs
@@ -15,14 +15,15 @@ public class FlyingEnemyBehavior : MonoBehaviour
 	public float attackMinTime = 1f; // Attack Time Minimums and Maximums
 	public float attackMaxTime = 4f;
 	public GameObject beam; // refrences it's beam object
+	private Direction direction;
 
 	private float attackTime = 0f; //	current attack time
 	private GameObject player; // handle on players MegaMan
 	private Animator anim;
 	private bool enemyActive;
 	private bool firstDash = false; // first dash handle for the inital jump towards player
-	private float explosionTime = 0f;
-	private Animator explosionAnim = null;
+	//private float explosionTime = 0f;
+	//private Animator explosionAnim = null;
 	private float yDifference = 0f;
 
 	public float speed = 1.25f; // horizontal speed
@@ -95,7 +96,7 @@ public class FlyingEnemyBehavior : MonoBehaviour
 		}
 	}
 
-	void DeathDriver ()
+	/*void DeathDriver ()
 	{
 		// drives the explosions
 		if (Time.time >= explosionTime)
@@ -107,26 +108,28 @@ public class FlyingEnemyBehavior : MonoBehaviour
 			// after the explosion animation changes, it will change the next anim's position around the object
 			explosionTime = Time.time + 0.9f;
 		}
-	}
+	}*/
 
 	void Movement ()
 	{
 
 		// Checks if it should move left or right towards player
-		if (transform.position.x - player.transform.position.x > 0)
+		if (transform.position.x - player.transform.position.x > 0.01f)
 		{
 			move = -1;
 			transform.eulerAngles = new Vector3 (0, -180, 0);
+			direction = Direction.LEFT;
 		}
 		else
 		{
 			move = 1;
 			transform.eulerAngles = new Vector3 (0, 0, 0);
+			direction = Direction.RIGHT;
 		}
 
 		// Checks if enemy is too high for player to hit - moves up and down
 		yDifference = transform.position.y - player.transform.position.y;
-		if (yDifference > 1.1f)
+		if (yDifference > 1.0f)
 		{
 			vSpeed = -0.1f;
 		}
@@ -149,7 +152,10 @@ public class FlyingEnemyBehavior : MonoBehaviour
 			attackTime = Time.time + Random.Range (attackMinTime, attackMaxTime);
 			//anim.SetTrigger ("Attack");
 			Vector3 beamRotation = new Vector3 (transform.rotation.x, transform.rotation.y, 90);
-			Instantiate (beam, new Vector3 (transform.position.x, transform.position.y - 0.04f, 0f), Quaternion.Euler (beamRotation));
+			var eBullet = Instantiate (beam, new Vector3 (transform.position.x, transform.position.y - 0.04f, 0f), Quaternion.Euler (beamRotation)) as GameObject;
+			print (eBullet);
+			eBullet.GetComponent<EnemyBullet> ().bulletDirection = direction;
+			print ("attack");
 		}
 	}
 
